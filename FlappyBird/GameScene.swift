1829e78f5bf8529e16b286b65ab5ac1c7f34e9e8
Abhigyan <abhigyan2311@gmail.com>
2015-12-27 20:24:42 +0530

    Initialized

---
+++ FlappyBird/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  FlappyBird
+//
+//  Created by Abhigyan Singh on 26/12/15.
+//  Copyright (c) 2015 Abhigyan Singh. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    
+          
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
